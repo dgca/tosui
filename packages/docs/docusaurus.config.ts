3562379c07cfad6e523863de352c7e6d76c8d9ff
dgca <dgca@users.noreply.github.com>
2025-12-24 17:17:11 -0300

    GH pages

--- packages/docs/docusaurus.config.ts
+++ packages/docs/docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://tosui.dev",
+  url: "https://dgca.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/tosui/",
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "tosui", // Usually your GitHub org/user name.
   projectName: "tosui", // Usually your repo name.
+  organizationName: "dgca", // Usually your GitHub org/user name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
 
